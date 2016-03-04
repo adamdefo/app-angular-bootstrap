aae5a2b453a8d652a7b1116906ce070d4c17b2d9
PVyazovcev <DK2929vb11>
2016-03-04 12:49:17 +0400

    Чиста от личшего

--- app/services/TestService.js
+++ app/services/TestService.js
@@ -1,10 +1,12 @@
+var app = angular.module('FuckApp', []);
 
-angular
-  .module('FuckApp', [])
-  .controller('MainCtrl', function ($scope, $rootScope, $location, $q, $http) {
+
+app.controller('MainCtrl', function ($scope, $rootScope, $location, $q, $http) {
     $scope.location = $location.path();
-  })
-  .factory('Factory', function() {
+
+});
+
+app.factory('ChatList', function() {
     return [
       { author: 'Вася Пупкин', message: 'Как записаться в очередь?' },
       { author: 'Вася Пупкин', message: 'Как записаться в очередь?' },
@@ -15,4 +17,4 @@ angular
       { author: 'Вася Пупкин', message: 'Как записаться в очередь?' },
       { author: 'Вася Пупкин', message: 'Как записаться в очередь?' }
     ];
-  });
+});
